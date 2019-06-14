7bd9214b3d3de1688264caaae9f3165a0754253e
domw30 <domw30@gmail.com>
2019-06-14 16:07:56 +0100

    update repository

---
+++ stargazerV3/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  stargazerV3
+//
+//  Created by Student on 13/06/2019.
+//  Copyright © 2019 Dominic. All rights reserved.
+//
+
+import UIKit
+import ARKit
+import SceneKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = .horizontal
+        config.worldAlignment = .gravityAndHeading
+        
+        sceneView.session.run(config)
+        
+        addSphere(material: "texture.jpg")
+        addSphere(material: "sun.jpg")
+        addSphere(material: "moon_material.jpeg")
+        addSphere(material: "moon_material.jpeg")
+
+        addCoordinates(text: "NORTH", x: 0 , y: 0, z: -100, rotation: 0 )
+        addCoordinates(text: "SOUTH", x: 0, y: 0, z: 100, rotation: 10 )
+        addCoordinates(text: "EAST", x: 100, y: 0, z: 0, rotation: 5 )
+        addCoordinates(text: "WEST", x: -100, y: 0, z: 0, rotation: -5 )
+      
+    }
+    
+    func addSphere(material: String){
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.25))
+        sphere.position = SCNVector3(Float.random(in: 0 ..< 5),1 ,Float.random(in: 0 ..< 5))
+        sphere.geometry?.firstMaterial?.diffuse.contents = material
+        sceneView.scene.rootNode.addChildNode(sphere)
+    }
+    
+    func addCoordinates(text: String, x: Int, y: Int, z: Int, rotation: CGFloat) {
+        let text = SCNNode(geometry: SCNText(string: text, extrusionDepth: 1))
+        text.position = SCNVector3(x, y, z )
+        text.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        text.runAction(SCNAction.rotateBy(x: 0, y: rotation, z: 0, duration: 0))
+        sceneView.scene.rootNode.addChildNode(text)
+    }
+}
+
