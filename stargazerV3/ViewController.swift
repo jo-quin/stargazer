445dc935cad65667609740b0fed60aab10248bb0
domw30 <domw30@gmail.com>
2019-06-15 14:13:17 +0100

    seperate star and planet functions

--- stargazerV3/ViewController.swift
+++ stargazerV3/ViewController.swift
@@ -23,10 +23,15 @@ class ViewController: UIViewController {
         
         sceneView.session.run(config)
         
-        addSphere(material: "sun.jpg", x: 0 , y: 0, z: -90, rotation: 0 )
-        addSphere(material: "moon_material", x: 90 , y: 0, z: 50, rotation: 5 )
-        addSphere(material: "sun.jpeg", x: 90 , y: 0, z: 0, rotation: 5 )
-        addSphere(material: "sun.jpg", x: -90 , y: 0, z: 0, rotation: -5 )
+        addStar(material: "texture.jpg")
+        addStar(material: "sun.jpg")
+        addStar(material: "moon_material.jpeg")
+        addStar(material: "moon_material.jpeg")
+        
+        addPlanet(material: "sun.jpg", x: 0 , y: 0, z: -90, rotation: 0 )
+        addPlanet(material: "moon_material", x: 90 , y: 0, z: 50, rotation: 5 )
+        addPlanet(material: "sun.jpeg", x: 90 , y: 0, z: 0, rotation: 5 )
+        addPlanet(material: "sun.jpg", x: -90 , y: 0, z: 0, rotation: -5 )
 
         addCoordinates(text: "NORTH", x: 0 , y: 0, z: -100, rotation: 0 )
         addCoordinates(text: "SOUTH", x: 0, y: 0, z: 100, rotation: 10 )
@@ -35,7 +40,14 @@ class ViewController: UIViewController {
       
     }
     
-    func addSphere(material: String, x: Int, y: Int, z: Int, rotation: CGFloat){
+    func addStar(material: String){
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.25))
+        sphere.position = SCNVector3(Float.random(in: 0 ..< 5),1 ,Float.random(in: 0 ..< 5))
+        sphere.geometry?.firstMaterial?.diffuse.contents = material
+        sceneView.scene.rootNode.addChildNode(sphere)
+    }
+    
+    func addPlanet(material: String, x: Int, y: Int, z: Int, rotation: CGFloat){
         let sphere = SCNNode(geometry: SCNSphere(radius: 10))
         let rotate = SCNAction.rotateBy(x: 0, y: 0.5, z: 0, duration: 1.0)
         let continuedRotate = SCNAction.repeatForever(rotate)
@@ -43,7 +55,6 @@ class ViewController: UIViewController {
         sphere.geometry?.firstMaterial?.diffuse.contents = material
         sphere.runAction(continuedRotate)
         sceneView.scene.rootNode.addChildNode(sphere)
-        
     }
     
     func addCoordinates(text: String, x: Int, y: Int, z: Int, rotation: CGFloat) {
